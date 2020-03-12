3febeeab9612250e59d775fcc8597c24e41cc305
Georgina Da Luz <daluz.georgina@gmail.com>
2020-03-12 20:48:38 -0300

    Agregue data a empVajilla

--- src/EmpVajilla.java
+++ src/EmpVajilla.java
@@ -1,9 +1,15 @@
+import java.util.List;
+
 public class EmpVajilla extends Empleado {
     public EmpVajilla(String informacionPersonal, Integer sueldo) {
         super(informacionPersonal, sueldo);
     }
-
+protected List<String> queRopmio;
     protected Integer cantidadVajillaRota;
+
+    public List<String> getQueRopmio() {
+        return queRopmio;
+    }
 
     public Integer getCantidadVajillaRota() {
         return cantidadVajillaRota;
